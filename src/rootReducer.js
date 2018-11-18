1d9c6b471bc4953c4b7875590edf585aa17d2bc1
Marvin Clerge <clergemarvin@gmail.com>
2018-11-17 21:20:13 -0500

    rootReducer commented

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,3 +1,4 @@
+// Inital data to start store with
 let initalState = {
   user: null,
   challenges: [],
@@ -7,31 +8,37 @@ let initalState = {
 
 export default function rootReducer(state = initalState, action){
   switch (action.type) {
+    // Add user information to store
     case "LOGIN":
       return Object.assign({} , state, {
         user: action.payload
       })
 
+    // Remove user information to store
     case "LOGOUT":
       return Object.assign({} , state, {
         user: null
       })
 
+    // Add challenges information to store
     case "CHALLENGES":
       return Object.assign({} , state, {
         challenges: action.payload
       })
 
+    // Add and set specific challenge information to store
     case "CHALLENGE":
       return Object.assign({}, state, {
         challenge: action.payload
       })
 
+    // Set status of user/challenge association to store
     case "USER_CHALLENGE":
       return Object.assign({}, state, {
         userChallenge: action.payload
       })
 
+    // Do not modifiy store if no reducers above were called
     default:
       return state;
   }
